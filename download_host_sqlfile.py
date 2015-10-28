92c6e3986a2297cb16fd3dd449d9e1c2a86135bf
Yao-Yuan Mao <yymao@users.noreply.github.com>
2015-10-27 20:29:26 -0400

    Update download_host_sqlfile.py
    code clean up

--- download_host_sqlfile.py
+++ download_host_sqlfile.py
@@ -2,9 +2,6 @@
 ##################################################
 # READ host list, print sql query
 # 
-#  > import saga
-#  > saga.()
-#
 # USES Dan FM's
 # https://github.com/dfm/casjobs
 #
@@ -20,186 +17,102 @@
 #
 # MG 7/2014
 ##################################################
+__all__ = ['run_query', 'run_casjob', 'construct_sdss_query']
 
-import numpy as np
-from astropy.io import ascii
-from astropy import units as u
-import requests
+import time
 import os
+from textwrap import dedent
+import numpy as np
+from astropy.table import Table
+from astropy import units as u
 
 
-path = os.environ['SAGADIR']
+def get_google_csv_url(key, gid):
+    return 'https://docs.google.com/spreadsheets/d/{key}/export?format=csv&gid={gid}'.format(**locals())
 
 
-# GOOGLE DOCUMENTS URLs
-SAGA_URLS = {'Host_noflags':'https://docs.google.com/a/yale.edu/spreadsheets/d/1b3k2eyFjHFDtmHce1xi6JKuj3ATOWYduTBFftx5oPp8/export?format=csv&gid=448084634',
-                 'Obs_Hosts': 'https://docs.google.com/spreadsheet/pub?key=0AggNS3_oqq91dHdVZ1J3cVZrRWVjcmkyeUF1WjVNQ1E&output=csv&gid=0',
-                 'Remove':'http://docs.google.com/spreadsheets/d/1Y3nO7VyU4jDiBPawCs8wJQt2s_PIAKRj-HSrmcWeQZo/export?format=csv&gid=1379081675'}
-
-
-#################################################################
-def run_query():
+def run_query(flagged_obs_hosts=False):
     """
     Send casjob queries for each host
 
     Parameters
     ----------
-    None  
-
+    flagged_obs_hosts : bool, optional
 
     Returns
     -------
-    Downloads Flag0 host files into sql files located in SAGADIR/hosts
+    Downloads host files into sql files located in SAGADIR/hosts
 
     """
 
-	# READ HOST LIST FROM GOOGLE DOCS
-    csvurl = SAGA_URLS.get('Host_noflags', None)
-    res = requests.get(csvurl)
-    csv = res.content.split('\n')[1:]
-    res.close() 
-    hostdata = ascii.read(csv)
-
-
-	# FOR EACH HOST, DOWNLOAD SQL QUERY
-    for host in hostdata:
-		nid  = host['col1']
-		print nid
-		if nid != -1:
-
-			nra  = host['col2']
-			ndec = host['col3']
-
-	      # CREATE SQL QUERY
-			qry = construct_sdss_query(nid,nra,ndec, 1.0)
-			print
-			print qry
-
-
-		  # RUN CASJOB.  NEED TO DEFINE CASJOBS_WSID and CASJOBS_PW in PATH
-			cjob = casjobs.CasJobs(base_url='http://skyserver.sdss3.org/casjobs/services/jobs.asmx')
-
-			out = 'sql_nsa'+str(nid)
-			outfits = out+'.fits'
-
-		  # IF FILE DOESN"T ALREADY EXIST, SUBMIT JOB TO CAS
-			if not os.path.isfile(outfits):
-		   		job_id = cjob.submit(qry, context='DR10')
-
-			  # WAIT FOR JOB TO FINISH
-				time.sleep(90)
-			   	code = 0 
-			   	code, status  = cjob.status(job_id)
-			   	print code, status
-				while (code != 5):
-					time.sleep(60)
-					code, status  = cjob.status(job_id)
-					print code, status
-
-
-			   # DOWNLOAD FILE AND REMOVE FROM MYDB LISTING (SAVE SPACE)
-				print 'downloading ',outfits
-				cjob.request_and_get_output(out, 'FITS', outfits)
-				cjob.drop_table(out)
-
-
-#################################################################
-
-def run_query_nonFlag0():
-    """
-    RUN SQL QUERY FOR NON-FLAG0 hosts
-    Based on non-flag0 hosts in OBS_HOST google docs
-
-    """
-
+    if flagged_obs_hosts:
+        url = get_google_csv_url('1GJYuhqfKeuJr-IyyGF_NDLb_ezL6zBiX2aeZFHHPr_s', 0)
+        nsa_col = 'NSA'
+    else:
+        url = get_google_csv_url('1b3k2eyFjHFDtmHce1xi6JKuj3ATOWYduTBFftx5oPp8', 448084634)
+        nsa_col = 'NSAID'
+    
     # READ HOST LIST FROM GOOGLE DOCS
-    csvurl = SAGA_URLS.get('Obs_Hosts', None)
-    res = requests.get(csvurl)
-    csv = res.content.split('\n')[1:]
-    res.close() 
-    hostdata = ascii.read(csv)
-
-
+    hostdata = Table.read(url, format='ascii.csv')
 
     # FOR EACH HOST, DOWNLOAD SQL QUERY
     for host in hostdata:
-        flag  = host['col8']   # SELECT ONLY NON_FLAG0
-        print flag
-        if flag != 0:
+        if flagged_obs_hosts and not host['Host Flag']:
+            continue
+        nid = host[nsa_col]
+        if nid == -1:
+            continue
+        print nid
 
-            nra  = host['col4']
-            ndec = host['col5']
-            nid  = host['col3']
-
-          # CREATE SQL QUERY
-            qry = construct_sdss_query(nid,nra,ndec, 1.0)
-            print
-            print qry
-
-
-          # RUN CASJOB.  NEED TO DEFINE CASJOBS_WSID and CASJOBS_PW in PATH
-            cjob = casjobs.CasJobs(base_url='http://skyserver.sdss3.org/casjobs/services/jobs.asmx')
-
-            out = 'sql_nsa'+str(nid)
-            outfits = out+'.fits'
-
-          # IF FILE DOESN"T ALREADY EXIST, SUBMIT JOB TO CAS
-            if not os.path.isfile(outfits):
-                job_id = cjob.submit(qry, context='DR10')
-
-              # WAIT FOR JOB TO FINISH
-                time.sleep(90)
-                code = 0 
-                code, status  = cjob.status(job_id)
-                print code, status
-                while (code != 5):
-                    time.sleep(60)
-                    code, status  = cjob.status(job_id)
-                    print code, status
+        # CREATE SQL QUERY
+        qry = construct_sdss_query(nid, host['RA'], host['Dec'])
+        print qry
+        run_casjob(qry, 'sql_nsa{}'.format(nid))
 
 
-               # DOWNLOAD FILE AND REMOVE FROM MYDB LISTING (SAVE SPACE)
-                print 'downloading ',outfits
-                cjob.request_and_get_output(out, 'FITS', outfits)
-                cjob.drop_table(out)
+def run_casjob(query, outname):
+    if not all(k in os.environ for k in ('CASJOBS_WSID', 'CASJOBS_PW')):
+        raise ValueError('You are not setup to run casjobs')
+    
+    SAGA_DIR = os.getenv('SAGADIR', os.curdir)
+    
+    cjob = casjobs.CasJobs(base_url='http://skyserver.sdss3.org/casjobs/services/jobs.asmx')
+    outfits = os.path.join(SAGA_DIR, outname + '.fits')
+
+    # IF FILE DOESN"T ALREADY EXIST, SUBMIT JOB TO CAS
+    if not os.path.isfile(outfits):
+        job_id = cjob.submit(query, context='DR10')
+        code = None
+        while code != 5:
+            time.sleep(60)
+            code, status = cjob.status(job_id)
+            print code, status
+
+        print 'downloading', outfits
+        cjob.request_and_get_output(outname, 'FITS', outfits)
+        cjob.drop_table(outname)
 
 
-
-#################################################################
-
-def construct_sdss_query(outname, ra, dec, radius=1*u.deg):
+def construct_sdss_query(outname, ra, dec, radius=1.0):
     """
     Generates the query to send to the SDSS to get the full SDSS catalog around
     a target.
 
     Parameters
     ----------
+    outname : string
     ra : `Quantity` or float
         The center/host RA (iconn degrees if float)
     dec : `Quantity` or float
         The center/host Dec (in degrees if float)
     radius : `Quantity` or float
         The radius to search out to (in degrees if float)
-    into : str or None
-        The name of the table to construct in your `mydb` if you want to use
-        this with CasJobs, or None to have no "into" in the SQL. This also
-        adjust other parts of the query a little to work with CasJobs instead
-        of the direct query.
-    magcut : 2-tuple or None
-        if not None, adds a magnitude cutoff.  Should be a 2-tuple
-        ('magname', faintlimit). Ignored if None.
-
-
+    
     Returns
     -------
     query : str
         The SQL query to send to the SDSS skyserver
-
-
     """
-    from textwrap import dedent
-
-
 
     query_template = dedent("""
     SELECT  p.objId  as objID,
@@ -230,23 +143,21 @@ def construct_sdss_query(outname, ra, dec, radius=1*u.deg):
         AND (flags & dbo.fPhotoFlags('BAD_COUNTS_ERROR')) = 0 
     """)
 
+    if isinstance(ra, u.Quantity):
+        ra = ra.to(u.deg).value
 
+    if isinstance(dec, u.Quantity):
+        dec = dec.to(u.deg).value
 
-    if isinstance(ra, float):
-        ra = ra*u.deg
-    ra = ra.to(u.deg).value
-
-    if isinstance(dec, float):
-        dec = dec*u.deg
-    dec = dec.to(u.deg).value
-
-    if isinstance(radius, float):
-        radius = radius*u.deg
-    radarcmin = radius.to(u.arcmin).value
+    if isinstance(radius, u.Quantity):
+        radarcmin = radius.to(u.arcmin).value
+    else:
+        radarcmin = (radius*u.deg).to(u.arcmin).value
 
     # ``**locals()`` means "use the local variable names to fill the template"
     return query_template.format(**locals())
 
 
-
-
+if __name__ == '__main__':
+    run_query(False)
+    run_query(True)
