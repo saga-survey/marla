a392a93373472afaf179fceb147d74eb3c48de19
Marla Geha <marlageha@Facultys-MacBook-Pro-2.local>
2016-04-12 11:02:11 -0400

    created plot

--- mk_paper/plot_Pclass_specz.py
+++ mk_paper/plot_Pclass_specz.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 from astropy import table
 from astropy.table import Table
 
+from astroML.plotting import scatter_contour
 from astroML.plotting import setup_text_plots
 setup_text_plots(fontsize=9, usetex=True)
 
@@ -28,47 +29,72 @@ def plot_Pclass_specz():
 
 
 	# READ SPECTRA
-	file = SAGA_DIR +'/data/saga_spectra_dirty.fits.gz'
+	file = SAGA_DIR +'/data/saga_spectra_clean.fits.gz'
 	allspec = Table.read(file)
+
+
+
+	lowz1 = allspec['SATS'] == 2
+	lowz2 = allspec['SPEC_Z'] < 0.02
+	lowz=lowz1&lowz2
 
 	sats = allspec['SATS'] == 1
 
+	b = allspec['r'] < 17.7
+	f = allspec['r'] >= 17.7
+
 	# FIND UNIQUE SAGANAMES AND ORDER BY NSATS
-	msk1          = allspec['ZQUALITY'] >= 3
-	msk2          = allspec['r']  > 17.7
+	
+
+	fig = plt.figure(figsize=(7, 5))
+	fig.subplots_adjust(left=0.1, right=0.95, wspace=0.05,
+                    bottom=0.1, top=0.95, hspace=0.05)
 
 
-	msk  = msk1 & msk2
-	sagaspec     = allspec[msk]
-	ssats = sagaspec['SATS'] == 1
+	thr=500
+	##############################
+	ax = fig.add_subplot(2, 1, 1)
+#	scatter_contour(allspec['PROBABILITY_CLASS1'],allspec['SPEC_Z'], log_counts=True, ax=ax,
+#                histogram2d_args=dict(bins=20),
+#                plot_args=dict(marker=',', linestyle='none', color='black'),
+#                contour_args=dict(cmap=plt.cm.bone))
+
+	ax.plot(allspec['PROBABILITY_CLASS1'][b],allspec['SPEC_Z'][b],'.k',markersize=2,label='All Galaxies')
+	ax.plot(allspec['PROBABILITY_CLASS1'][b&lowz],allspec['SPEC_Z'][b&lowz],'.b',markersize=5,label='$0.001 < z < 0.02$')
+	ax.plot(allspec['PROBABILITY_CLASS1'][b&sats],allspec['SPEC_Z'][b&sats],'.r',markersize=7,label = 'Satellites')
 
 
-
-#	plt.subplot(2,1,1)
-	fig = plt.figure(figsize=(7, 3))
-	fig.subplots_adjust(bottom=0.15, top=0.9,left=0.12, right=0.95, wspace=0.3)
-
-	ax = fig.add_subplot(121)
-	ax.plot(allspec['PROBABILITY_CLASS1'],allspec['SPEC_Z'],'.',markersize=0.8)
-	ax.plot(allspec['PROBABILITY_CLASS1'][sats],allspec['SPEC_Z'][sats],'o',markersize=1)
 	ax.set_xlabel('Pclass')
 	ax.set_ylabel('Spec z')
-	ax.set_ylim(1e-3,5)
+	ax.set_ylim(1e-3,1.2)
+	ax.set_xlim(1e-10,1.1)
 	ax.set_yscale('log')
 	ax.set_xscale('log')
+	ax.get_xaxis().set_visible(False)
+	ax.text(2e-10,2e-3,'Bright Galaxies, r $<$ 17.7')
 
 
-	ax = fig.add_subplot(122)
-	ax.plot(sagaspec['PROBABILITY_CLASS1'],sagaspec['SPEC_Z'],'.',markersize=0.8)
-	ax.plot(sagaspec['PROBABILITY_CLASS1'][ssats],sagaspec['SPEC_Z'][ssats],'o',markersize=1)
+	plt.legend(loc=1,fontsize=9)
+
+
+	##############################
+	ax = fig.add_subplot(2, 1, 2)
+	ax.plot(allspec['PROBABILITY_CLASS1'][f],allspec['SPEC_Z'][f],'.k',markersize=2,label='All Galaxies')
+
+	ax.plot(allspec['PROBABILITY_CLASS1'][f&lowz],allspec['SPEC_Z'][f&lowz],'.b',markersize=5,label='$0.001 < z < 0.02$')
+	ax.plot(allspec['PROBABILITY_CLASS1'][f&sats],allspec['SPEC_Z'][f&sats],'.r',markersize=7,label = 'Satellites')
+
 	ax.set_xlabel('Pclass')
 	ax.set_ylabel('Spec z')
-	ax.set_ylim(1e-3,5)
+	ax.set_ylim(1e-3,1.2)
+	ax.set_xlim(1e-10,1.1)
 	ax.set_yscale('log')
 	ax.set_xscale('log')
+	ax.text(2e-10,2e-3,'Faint Galaxies, r $>$ 17.7')
+
 
 	plt.show()
-	plt.savefig('Pclass.v.specz.pdf')
+	plt.savefig('fig_Pclass.pdf')
 
 if __name__ == '__main__':
     create_saga_spectra()
